922023a19fd11740675a0f982d6f9d8c2e4aea0c
Muadz Yamani <muadzyamani@gmail.com>
2025-07-03 20:59:15 +0800

    Add Scanning class to handle user input and iterations

---
+++ practice/src/Scanning.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Scanning {
+    public static void main(String[] args) throws Exception {
+        int i = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of iterations: ");
+        int times = Integer.valueOf(scanner.nextLine());
+
+        while (i < times) {
+            i++;
+            System.out.println(i);
+        }
+
+        System.out.println("Ready!");
+    }
+}
